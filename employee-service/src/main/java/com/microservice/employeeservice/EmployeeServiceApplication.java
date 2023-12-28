027f9fd703908b59e7acb6b1def0fccc05d32179
Omkar510 <omkarsuvare49@gmail.com>
2023-12-28 16:27:00 +0530

    Employee Service Changes

--- employee-service/src/main/java/com/microservice/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/microservice/employeeservice/EmployeeServiceApplication.java
@@ -3,6 +3,8 @@ package com.microservice.employeeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -13,10 +15,10 @@ public class EmployeeServiceApplication {
 	// 	return new RestTemplate();
 	// }
 
-	// @Bean
-	// WebClient webClient() {
-	// 	return WebClient.builder().build();
-	// }
+	@Bean
+	WebClient webClient() {
+		return WebClient.builder().build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
